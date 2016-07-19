cd167fa952473e6713f398915e3bba181bd3a09f
jonpitch <jon.pitcherella@gmail.com>
2016-07-19 15:11:48 -0400

    basic flow meter support #3

--- index.js
+++ index.js
@@ -35,6 +35,51 @@ Display.prototype = {
   }
 }
 
+// setup flow meter facade
+var FlowMeter = function(device, display) {
+  this._device = device;
+  this._display = display;
+
+  // total pulses from flow meter
+  var pulses = 0;
+
+  // pulses per session - gets reset
+  var sessionPulses = 0;
+
+  // state of flow meter
+  var isOpen = false;
+
+  // how many pulses occur before 1L of liquid
+  var pulsesPerLiter = 450;
+
+  // ounces in a Liter
+  var ouncesPerLiter = 33.814;
+
+  // pules per ounce
+  var pulsesPerOunce = 13.308;
+
+  this._device.on('change', function() {
+    pulses++;
+    sessionPulses++;
+    isOpen = true;
+
+    var currentSession = sessionPulses;
+    setTimeout(function() {
+      if (currentSession === sessionPulses) {
+        var ounces = Math.round((sessionPulses / pulsesPerOunce) * 100) / 100;
+        display.write('poured: ' + ounces + 'oz');
+
+        // reset
+        sessionPulses = 0;
+        isOpen = false;
+        setTimeout(function() {
+          display.clear();
+        }, 500);
+      }
+    }, 1000);
+  });
+}
+
 // setup board
 board.on('ready', function() {
   // initialize display
@@ -49,9 +94,14 @@ board.on('ready', function() {
   // clear display just in case
   display._device.update();
 
+  // setup flow meter
+  // TODO dynamic from configuration?
+  var f = new five.Sensor.Digital('P1-22');
+  var meter = new FlowMeter(f, display);
+
   // setup toggle
   var currentState = 0;
-  var states = ['off', 'temp'];
+  var states = ['off', 'temp', 'flow'];
 
   var toggle = new five.Button('P1-36');
   toggle.on('up', function() {
@@ -68,6 +118,9 @@ board.on('ready', function() {
     } else if (state === 'temp') {
       display.on();
       display.write('temp');
+    } else if (state === 'flow') {
+      display.on();
+      display.write('flow');
     }
   });
 
