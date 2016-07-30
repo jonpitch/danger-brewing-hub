7d80589681c06d78b4118bd15aab689c25705596
jonpitch <jon.pitcherella@gmail.com>
2016-07-30 10:12:17 -0400

    es6 support, updating diagram for flow meters and updating readme #7 #3

--- index.js
+++ index.js
@@ -1,130 +1,157 @@
-var raspi = require('raspi-io');
-var five = require('johnny-five');
-var oled = require('oled-js');
-var font = require('oled-font-5x7');
-var board = new five.Board({
+import raspi from 'raspi-io';
+import five from 'johnny-five';
+import oled from 'oled-js';
+import font from 'oled-font-5x7';
+
+// setup board
+const board = new five.Board({
   io: new raspi()
 });
 
-// setup display facade
-var Display = function(device) {
-  this._device = device;
-}
+//
+/**
 
-Display.prototype = {
-  // turn display on
-  on: function() {
+*/
+class Display {
+
+  // TODO move height, width and address of display to config
+  // TODO make display hardware optional.
+  //  if someone wants to build the hub without display, fallback to console
+  constructor(board, five) {
+    const hardware = new oled(board, five, {
+      width: 128,
+      height: 32,
+      address: 0x3C
+    });
+
+    this._device = hardware;
+
+    // setup toggle
+    this._currentState = 0;
+    this._states = [
+      'off',
+      'flow',
+      'temp-lower',
+      'temp-upper'
+    ];
+
+    // TODO pin is configurable
+    const toggle = new five.Button('P1-36');
+    toggle.on('up', () => {
+      let next = this._currentState + 1;
+      if (next >= this._states.length) {
+        next = 0;
+      }
+
+      if (next === 0) {
+        // display is off
+        this.clear();
+        this.off();
+
+      } else if (next === 1) {
+        // display set to flow meters
+        this.on();
+        this.write('temp');
+
+      } else if (next === 2) {
+        // display set to lower temperature
+        this.write('lower');
+
+      } else if (next === 3) {
+        // display set to upper temperature
+        this.write('upper');
+
+      }
+
+      this._currentState = next;
+    });
+
+    // clear display on initialization - just in case
+    this._device.update();
+  }
+
+  // trun display on
+  on() {
     this._device.turnOnDisplay();
-  },
+  }
 
   // turn display off
-  off: function() {
+  off() {
     this._device.turnOffDisplay();
-  },
+  }
 
   // clear display
-  clear: function() {
+  clear() {
     this._device.clearDisplay();
-  },
+  }
 
   // write string to screen
-  write: function(text) {
+  write(text) {
     this.clear();
     this._device.setCursor(1, 1);
     this._device.writeString(font, 1, text, 1, true, 2);
   }
+
 }
 
-// setup flow meter facade
-var FlowMeter = function(device, display) {
-  this._device = device;
-  this._display = display;
+/*
 
-  // total pulses from flow meter
-  var pulses = 0;
+*/
+class FlowMeter {
 
-  // pulses per session - gets reset
-  var sessionPulses = 0;
+  constructor(device, display) {
+    this._device = device;
+    this._display = display;
 
-  // state of flow meter
-  var isOpen = false;
+    // total pulses from flow meter
+    var pulses = 0;
 
-  // how many pulses occur before 1L of liquid
-  var pulsesPerLiter = 450;
+    // pulses per session - gets reset
+    var sessionPulses = 0;
 
-  // ounces in a Liter
-  var ouncesPerLiter = 33.814;
+    // state of flow meter
+    var isOpen = false;
 
-  // pules per ounce
-  var pulsesPerOunce = 13.308;
+    // may require calibration
+    const pulsesPerLiter = 450;
+    const ouncesPerLiter = 33.814;
+    const pulsesPerOunce = 13.308;
 
-  this._device.on('change', function() {
-    pulses++;
-    sessionPulses++;
-    isOpen = true;
+    this._device.on('change', () => {
+      pulses++;
+      sessionPulses++;
+      isOpen = true;
 
-    var currentSession = sessionPulses;
-    setTimeout(function() {
-      if (currentSession === sessionPulses) {
-        var ounces = Math.round((sessionPulses / pulsesPerOunce) * 100) / 100;
-        display.write('poured: ' + ounces + 'oz');
+      let currentSession = sessionPulses;
+      setTimeout(() => {
+        if (currentSession === sessionPulses) {
+          const ounces = Math.round((sessionPulses / pulsesPerOunce) * 100) / 100;
+          this._display.write(`poured: ${ounces} oz`);
 
-        // reset
-        sessionPulses = 0;
-        isOpen = false;
-        setTimeout(function() {
-          display.clear();
-        }, 500);
-      }
-    }, 1000);
-  });
+          // reset
+          sessionPulses = 0;
+          isOpen = false;
+          setTimeout(() => {
+            this._display.clear();
+          }, 500);
+        }
+      }, 1000);
+    });
+  }
 }
 
-// setup board
+// setup hub
 board.on('ready', function() {
   // initialize display
-  var displayDevice = new oled(board, five, {
-    width: 128,
-    height: 32,
-    address: 0x3C
-  });
-
-  var display = new Display(displayDevice);
-
-  // clear display just in case
-  display._device.update();
+  const display = new Display(board, five);
 
   // setup flow meter
   // TODO dynamic from configuration?
-  var f = new five.Sensor.Digital('P1-22');
-  var meter = new FlowMeter(f, display);
-
-  // setup toggle
-  var currentState = 0;
-  var states = ['off', 'temp', 'flow'];
-
-  var toggle = new five.Button('P1-36');
-  toggle.on('up', function() {
-    var next = currentState + 1;
-    if (next >= states.length) {
-      next = 0;
-    }
-
-    currentState = next;
-    var state = states[next];
-    if (state === 'off') {
-      display.clear();
-      display.off();
-    } else if (state === 'temp') {
-      display.on();
-      display.write('temp');
-    } else if (state === 'flow') {
-      display.on();
-      display.write('flow');
-    }
-  });
+  const f = new five.Sensor.Digital('P1-22');
+  const meter = new FlowMeter(f, display);
 
   // on shutdown
+  // TODO notify web app event occurred
   this.on('exit', function() {
     display.off();
   });
